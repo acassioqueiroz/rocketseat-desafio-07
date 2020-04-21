64d45c8fcac80356abd96b54f956b4cb5471a005
Acássio Queiroz <acassio@gmail.com>
2020-04-20 22:04:12 -0300

    First version of challange

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,23 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
     try {
-      // await api.post('/transactions/import', data);
+      data.append('file', uploadedFiles[0].file);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const moreUploadedFiles: FileProps[] = files.map(
+      (file: File): FileProps => {
+        return { file, name: file.name, readableSize: file.size.toString() };
+      },
+    );
+
+    setUploadedFiles(moreUploadedFiles);
   }
 
   return (
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number | string): string | null => {
+  const valueNumber =
+    typeof value === 'number' ? value : Number.parseFloat(value);
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(valueNumber);
+};
 
 export default formatValue;
